36a0fb8ab82fc19fe3414c91eccd4d6d6bdb5e92
Phạm Lê Tuấn Anh <tuananhlae@gmail.com>
2025-08-06 22:35:41 +0700

    Add web config

--- WebAPIStrain/WebAPIStrain/obj/Debug/net8.0/WebAPIStrain.AssemblyInfo.cs
+++ WebAPIStrain/WebAPIStrain/obj/Debug/net8.0/WebAPIStrain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6161299194301b98ff23060d12cae336e5f965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623a70a062ac68c712e19b1aa55e757d7c83bec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
